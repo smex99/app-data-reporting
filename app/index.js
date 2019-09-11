c0aef292a1c1fef6c4e201ec951151611762d702
AMEZIANE <amine.ameziane@mosaiclab.io>
2019-09-11 12:22:55 +0100

    unsused route disabled

--- app/index.js
+++ app/index.js
@@ -18,27 +18,16 @@ app.use('/api/bb8', require('./routes/bb8'));
 app.use('/api/focus', require('./routes/focus'));
 app.use('/api/report', require('./routes/report'));
 
-app.get('/image/:id', (req, res) => {
+app.get('/image/:filename', (req, res) => {
 	const { filename } = req.params;
 
-	var options = {
-		root: path.join(__dirname, 'public'),
-		dotfiles: 'deny',
-		headers: {
-			'x-timestamp': Date.now(),
-			'x-sent': true
-		}
-	};
-
-	res.sendFile(`${filename}`, options, error => {
+	res.sendFile(`/home/lake/Documents/rma_all_invoice/${filename}`, error => {
 		if (error) {
 			next(error);
 		} else {
-			console.log('Sent:', fileName);
+			console.log('Sent:', filename);
 		}
 	});
-
-	// res.sendFile(`/home/lake/Documents/rma_all_invoice/${filename}.jpeg`);
 });
 
 // Run the app on port 5000
--- app/routes/report.js
+++ app/routes/report.js
@@ -4,6 +4,7 @@ const AppController = require('../controllers/app');
 router.route('/').get(AppController.invoiceReadingResult);
 router.route('/prediction').get(AppController.predictionMetrics);
 router.route('/performance').get(AppController.performanceMetrics);
+router.route('/filename/:id').get(AppController.getfilenameByInvoiceId);
 // router.route('/count').get(AppController.readingClassCountPredicted);
 
 module.exports = router;
